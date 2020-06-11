b28ee1b882d6862123b6ae54c1e193c2f55546e6
Felipe <felipe@aireset.com.br>
2020-06-11 11:12:50 -0300

    troca de namespace

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-    Route::group(['namespace' => 'Zhiru\PagSeguro'], function () {
+    Route::group(['namespace' => 'Aireset\PagSeguro'], function () {
         Route::get('/pagseguro/session', 'PagSeguroController@session');
         Route::get('/pagseguro/javascript', 'PagSeguroController@javascript');
     });
